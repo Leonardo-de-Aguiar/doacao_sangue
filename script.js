a4715df82f6f2c0a2e9887f8834138bc1d323855
Leonardo-de-Aguiar <leonardo_n_aguiar@estudante.sesisenai.org.br>
2026-04-09 08:25:51 -0300

    add - fiz o js e o css

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("formulario_sangue").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+    let idade = document.getElementById("idade").value;
+    let cidade = document.getElementById("cidade").value;
+    let peso = document.getElementById("peso").value;
+    let estado = document.getElementById("estado").value;
+
+
+    if((nome.length < 3) && (!nome.includes (""))) return alert("Nome Inválido");
+    if((!email.includes ("@")) && (!email.includes("."))) return alert("Email Inválido");
+    if(telefone.length < 8) return alert("Telefone Inválido");
+    if(idade < 16) return alert("Idade Inválida");
+    if(cidade == ("")) return alert("Cidade Inválida");
+    if(peso < 50) return alert("Peso Inválido");
+    if(estado == ("")) return alert("Estado inválido");
+
+
+
+    document.getElementById("finalizar_cadastro").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
+});
